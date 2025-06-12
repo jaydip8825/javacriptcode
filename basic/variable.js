6223de24d1f7f51f581f992b158058b91b599d36
jaydip8825 <jaydip8825@gmail.com>
2025-06-12 10:05:52 +0000

    second

---
+++ basic/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "jaydip@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountStat;
+
+// accountId = 2
+
+
+accountEmail = "jay@gmail.com"
+accountPassword = "1245"
+accountCity = "Bangluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStat])
