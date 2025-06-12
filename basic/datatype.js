ff1983ca169671ae7a66eb5f1cb9b160447f8456
jaydip8825 <jaydip8825@gmail.com>
2025-06-12 10:38:34 +0000

    datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,26 @@
+"use strict"
+
+
+console.log(3 + 3);
+console.log("jaydip");
+
+
+let name = "jaydip"
+let age = 21
+let isLoggedIn = false
+
+
+
+// This is All Data Type
+// Number 2 to power 53
+//bigint
+//string 
+//boolean
+//Null => "Standalone value"
+//Undefines =>
+// symbol
+
+
+//object 
+
+console.log(typeof null);
